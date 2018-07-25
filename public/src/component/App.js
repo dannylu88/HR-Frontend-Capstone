72f88e37ef57e5b0f3c180d16fbd1bfa9fac1c0b
Danny Lu <dannylu8@outlook.com>
2018-07-24 22:32:43 -0700

    added array of photos but THIS COMMIT does NOT work.

--- public/src/component/App.js
+++ public/src/component/App.js
@@ -1,17 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-class App extends React.Component{
+class Gallery extends React.Component{
 	constructor(props){
 		super(props)
+	}
+
+	renderImage(imageUrl){
+		return (
+          <div>
+          	<img src = {imageUrl} />
+          </div>   
+	    )
 	}
 	render(){
 		return(
 
-			<div>
-				connected
+			<div className = 'gallery'>
+			  <div className = 'images'>
+			    {this.props.imageUrls.map((imageUrl, index) => (
+			    	this.renderImage(imageUrl)))}
+			  </div>
+		     		
 			</div>
-			)
+		 );
 	}
 }
 
-export default App;
+// Gallery.propTypes = {
+//   imageUrls: React.PropTypes.arrayOf(React.PropTypes.string).isRequired
+// };
+
+export default Gallery;
--- public/src/index.js
+++ public/src/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-import {render} from 'react-dom';
-import App from './component/App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Gallery from './component/App'
 
 // const app = document.getElementById('root')
 
-render(<App />, document.getElementById('root'));
+const app = document.getElementById('root');
+
+let imageUrls = [
+	".../Img/amazon-phone-1.png",
+	".../Img/amazon-phone-2.png",
+	".../Img/amazon-phone-3.png"
+];
+
+ReactDOM.render(<Gallery imageUrls = {imageUrls} />, app);
