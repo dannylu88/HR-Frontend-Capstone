8990f54f0c4728f26f9d782d0085cf9f2210117b
Danny Lu <dannylu8@outlook.com>
2018-07-30 16:52:11 -0700

    added 2 more tests but testing syntax with chai, chai-enzyme, jest are having conflicts

--- test/App.test.js
+++ test/App.test.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import App from '../public/src/component/App'
 import {shallow} from 'enzyme'
+import chai from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+
+chai.use(chaiEnzyme());
+
+global.chai = chai;
+global.expect = chai.expect;
 
 //const ImagesArray = ['image1.png', 'image2.png', 'image3.png']
 
 describe('App component', () =>{
-  it('state exist', () =>{
+  it('Initial image state exist and equal to empty string', () =>{
 		const wrapper = shallow(<App />)
 		const ImageState = wrapper.state().image
-		expect(ImageState).toBe('')
-		
-	})
+		expect(ImageState).toBe('');
+	});
+	
+	it('....', () => {
+		const wrapper = shallow(<App />)
+		expect(wrapper.find('.imageContainer')).to.have.lengthOf(0);
+		expect(wrapper.find(Image)).to.have.lengthOf(1);
+	});
+
 });
